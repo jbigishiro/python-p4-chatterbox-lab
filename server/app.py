0d3ef61b5bb0ed36d017243d72c1863ca47a18a1
Justin Bigishiro <justin.rvd@gmail.com>
2023-09-08 10:28:52 -0500

    finish lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,78 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_serialized = [msg.to_dict()for msg in messages]
+        response = make_response( messages_serialized, 200)
+        return response
+    
+    elif request.method == 'POST':
+        msg=request.get_json()
+        new_message = Message(
+            body = msg["body"],
+            username = msg["username"],
+        )
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        message_dict = new_message.to_dict()
+
+        response = make_response( message_dict, 201)
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if message == None:
+        response_body ={
+            "message": "This record does not exist in our database. Please try again."
+        }
+        response = make_response ( jsonify(response_body), 404)
+
+        return response
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+            response = make_response(message_dict, 200)
+            return response
+        
+        elif request.method == 'PATCH':
+            msg = request.get_json()
+            message.body = msg["body"]
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+
+            response = make_response(
+                message_dict,
+                200
+            )
+
+            return response
+        
+        elif request.method == 'DELETE':
+            message = Message.query.filter(Message.id == id).first()
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Message deleted."    
+            }
+
+            response = make_response(
+                response_body,
+                200
+            )
+
+            return response
+        
 
 if __name__ == '__main__':
     app.run(port=5555)
